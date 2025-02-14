c3546603ae9bac8b5e09a02e151e02a884240d4c
Kazu Yamamoto <kazu@iij.ad.jp>
2025-02-15 08:48:03 +0900

    defining nEnc

--- Crypto/HPKE.hs
+++ Crypto/HPKE.hs
@@ -39,9 +39,23 @@ module Crypto.HPKE (
 
     -- * Error
     HPKEError (..),
+
+    -- * Misc
+    nEnc,
 ) where
 
 import Crypto.HPKE.Context
 import Crypto.HPKE.ID
 import Crypto.HPKE.Setup
 import Crypto.HPKE.Types
+
+-- | Length of "enc", aka sender's public key.
+{- FOURMOLU_DISABLE -}
+nEnc :: KEM_ID -> Int
+nEnc DHKEM_P256_HKDF_SHA256   =  65
+nEnc DHKEM_P384_HKDF_SHA384   =  97
+nEnc DHKEM_P521_HKDF_SHA512   = 133
+nEnc DHKEM_X25519_HKDF_SHA256 =  32
+nEnc DHKEM_X448_HKDF_SHA512   =  56
+nEnc _                        =  0
+{- FOURMOLU_ENABLE -}
